7623ac77a7f002aac6c66c162cc221c845f4df2e
JulietaAmormino <84631150+JulietaAmormino@users.noreply.github.com>
2021-09-06 13:25:01 -0300

    Add files via upload

---
+++ EstacionElAleman/EstacionElAleman/Formularios/Frm_Escritorio.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstacionElAleman
+{
+    public partial class Frm_Escritorio : Form
+    {
+        string usuario = "";
+        string clave = "";
+        int id_usuario = 0;
+
+        public Frm_Escritorio()
+        {
+            InitializeComponent();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Frm_Escritorio_Load(object sender, EventArgs e)
+        {
+            Frm_Login login = new Frm_Login();
+            login.ShowDialog();
+            this.usuario = login.Pp_Usuario;
+            this.clave = login.Pp_clave;
+            login.Dispose();
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void empleadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void registrarPedidoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listadoDeCombustibleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listadoDeComprasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EstacionElAleman/EstacionElAleman/Formularios/Frm_Login.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstacionElAleman
+{
+    public partial class Frm_Login : Form
+    {
+        public string Pp_Usuario
+        {
+            get { return this.txt_Usuario.Text; }
+            set { this.txt_Usuario.Text = value; }
+        }
+        public string Pp_clave
+        {
+            get { return this.txt_clave.Text; }
+            set { this.txt_clave.Text = value; }
+        }
+
+        public Frm_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Frm_Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
